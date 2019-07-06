f763b767eb4f1b5cb128fd997290756f8f339d92
alifdev <matthias.alif@epitech.eu>
2019-07-06 20:13:44 +0200

    Home page done

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,57 @@
 <script>
-	export let name;
+  import { Router, Route, Link } from "svelte-routing";
+
+  import Home from "./components/Home.svelte";
+  import Room from "./components/Room.svelte";
+
+  export let url = "";
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  .app {
+    background: linear-gradient(160deg, #1c4891, #5abc95);
+    height: 100%;
+    font-size: 1em;
+    display: flex;
+    flex-direction: column;
+    overflow-y: auto;
+    justify-content: space-between;
+  }
+
+  .header {
+    display: flex;
+    justify-content: space-around;
+  }
+
+
+  .title {
+    border-bottom: solid 1px;
+    margin: auto;
+    padding: 3vh 1vw 1vh 1vw;
+    vertical-align: middle;
+    font-size: 3rem;
+  }
+
+  .footer {
+    text-align: right;
+    margin: 0 2vw 2vh 0;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<div class="app">
+  <div class="header">
+    <h1 class="title">
+      Flash
+      <i>by PerfectWeek</i>
+    </h1>
+  </div>
+
+  <Router {url}>
+    <div class="content">
+      <Route path="/" component={Home} />
+      <Route path="room" component={Room} />
+    </div>
+  </Router>
+
+  <div class="footer">© PerfectWeek 2019</div>
+</div>
---
+++ src/components/Home.svelte
@@ -0,0 +1,88 @@
+<script>
+  import { Link } from "svelte-routing";
+
+  let slotsFoundCount = 0;
+  const timeAverage = 27;
+
+  function increaseCount() {
+    slotsFoundCount += 1;
+  }
+</script>
+
+<style>
+  .home {
+    height: 100%;
+  }
+
+  .flex-component {
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+  }
+
+  .btn {
+    background-color: black;
+    color: white;
+    cursor: pointer;
+    font-size: 1.5em;
+    padding: 2vh 2vw 2vh 2vw;
+    margin-top: 2.5vh;
+  }
+
+  .home-content {
+    justify-content: space-around;
+    margin: 0 5vw 0 5vw;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  .headline {
+    font-size: 3em;
+    line-height: 150%;
+    margin: 0;
+  }
+
+  .right-part {
+    font-size: 2rem;
+  }
+
+  .infos {
+    list-style-type: none;
+    line-height: 120%;
+    padding: 0;
+  }
+
+  .question {
+    margin: 0;
+  }
+
+</style>
+
+<div class="home flex-component">
+  <div class="home-content flex-component">
+    <div class="left-part">
+      <h2 class="headline">
+        ORGANISEZ VOUS
+        <br />
+        GAGNEZ DU TEMPS
+        <br />
+        VIVEZ.
+      </h2>
+      <Link to="room">
+        <button class="create-room btn" on:click={increaseCount}>
+          TROUVER UN CRENEAU
+        </button>
+      </Link>
+    </div>
+    <div class="right-part">
+    <h2 class="question">Flash <i>by PerfectWeek</i>, c'est quoi ?</h2>
+      <ul class="infos">
+        <li>Synchronisation automatique de l'agenda</li>
+        <li>Une interface simple et efficace</li>
+        <li>La revolution de la creation d'evenements</li>
+        <li>L'expertise PerfectWeek</li>
+      </ul>
+    </div>
+  </div>
+
+</div>
